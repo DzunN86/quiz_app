cfa848cd2aacc41a64d9b01d926553d61007eab5
Dzun <nurroinz@gmail.com>
2024-03-13 08:15:51 +0700

    Update questions_screen.dart

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
   @override
-  State<QuestionsScreen> createState()  {
+  State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
 }
 
-
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    final currentQuestion = questions[0];
+    return Container(
+      margin: const EdgeInsets.all(40),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(currentQuestion.text,
+                style: const TextStyle(color: Colors.white),
+                textAlign: TextAlign.center),
+            const SizedBox(height: 30),
+            // loop through the answers and create a button for each
+            ...currentQuestion.getSuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {},
+              );
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
